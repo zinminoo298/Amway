80010455a43ba23369392e91cebd9779abdddd45
zinminoo298 <niznimoo26111997@gmail.com>
2021-02-09 13:53:06 +0700

    Added credential

--- app/src/main/java/com/example/amway/Database/DatabaseHandler.kt
+++ app/src/main/java/com/example/amway/Database/DatabaseHandler.kt
@@ -1,10 +1,47 @@
 package com.example.amway.Database
 
 import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.util.Log
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
 class DatabaseHandler(val context: Context) {
 
     companion object{
-        val database = "database.db"
+        const val database = "database.db"
+    }
+
+    fun openDatabase(): SQLiteDatabase {
+        val dbFile=context.getDatabasePath(database)
+        if (!dbFile.exists()) {
+            try {
+                val checkDB=context.openOrCreateDatabase(database, Context.MODE_PRIVATE, null)
+
+                checkDB?.close()
+                copyDatabase(dbFile)
+            } catch (e: IOException) {
+                throw RuntimeException("Error creating source database", e)
+            }
+
+        }
+        return SQLiteDatabase.openDatabase(dbFile.path, null, SQLiteDatabase.OPEN_READWRITE)
+    }
+
+    private fun copyDatabase(dbFile: File?) {
+        val `is`=context.assets.open(database)
+        val os= FileOutputStream(dbFile)
+
+        val buffer=ByteArray(1024)
+        while(`is`.read(buffer)>0) {
+            os.write(buffer)
+            Log.d("#DB", "writing>>")
+        }
+
+        os.flush()
+        os.close()
+        `is`.close()
+        Log.d("#DB", "completed..")
     }
 }
--- app/src/main/java/com/example/amway/Login.kt
+++ app/src/main/java/com/example/amway/Login.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.amway.Database.DatabaseHandler
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,10 +14,26 @@ class Login : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         val login = findViewById<Button>(R.id.btn_login)
+        val edtUser = findViewById<EditText>(R.id.edit_username)
+        val edtPsw = findViewById<EditText>(R.id.edit_password)
+
+        val db:DatabaseHandler = DatabaseHandler(this)
+//        db.openDatabase()
 
         login.setOnClickListener {
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
+
+            if(edtUser.text.toString()=="" || edtPsw.text.toString()==""){
+                Toast.makeText(this,"Please enter username and password",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                if(edtUser.text.toString()=="admin" && edtPsw.text.toString()=="123456"){
+                    val intent = Intent(this,MainActivity::class.java)
+                    startActivity(intent)
+                }
+                else{
+                    Toast.makeText(this,"Wrong password or username",Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }
